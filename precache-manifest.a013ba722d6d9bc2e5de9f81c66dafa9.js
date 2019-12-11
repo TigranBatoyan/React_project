be6ffb8effedaef77ce5fc7ac6d7d76534c1a2c1
TigranBatoyan <51286422+TigranBatoyan@users.noreply.github.com>
2019-12-11 23:51:36 +0400

    Updates

---
+++ precache-manifest.a013ba722d6d9bc2e5de9f81c66dafa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e881462167dbc7043fcc821bf9f70c09",
+    "url": "/React_project/index.html"
+  },
+  {
+    "revision": "e6e1885cd85274a43178",
+    "url": "/React_project/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e0c24d0015e0fa509c1b",
+    "url": "/React_project/static/css/main.09bd2a6e.chunk.css"
+  },
+  {
+    "revision": "e6e1885cd85274a43178",
+    "url": "/React_project/static/js/2.d3ccb874.chunk.js"
+  },
+  {
+    "revision": "e0c24d0015e0fa509c1b",
+    "url": "/React_project/static/js/main.4586d9c6.chunk.js"
+  },
+  {
+    "revision": "2e94f64c2e7ebaa8af6c",
+    "url": "/React_project/static/js/runtime-main.f1cf9491.js"
+  }
+]);
